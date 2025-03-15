bd72197814899a849a0b19c063d9a0369d68260f
John Ramsden <john@ubc.ramsdenj.ca>
2025-03-14 18:32:23 -0700

    Add error checking to script

--- scripts/run_workloads.sh
+++ scripts/run_workloads.sh
@@ -10,34 +10,39 @@ threads="$3"
 
 # Loop through all files in the directory
 for file in "$directory"/*; do
-	# Check if it's a file (not a directory)
-    if [ -f "$file" ]; then
-        filename=$(basename ${file})
-		# Split the filename into an array using comma as the delimiter
-		IFS=, read -ra params <<< "$filename"
+    # Check if it's a file (not a directory)
+    if ! [ -f "$file" ]; then
+        echo "ERROR: no such file '$file', skipping"
+        continue
+    fi
+    filename=$(basename ${file})
+    # Split the filename into an array using comma as the delimiter
+    IFS=, read -ra params <<< "$filename"
 
-		# Iterate over each key-value pair
-		for param in "${params[@]}"; do
-			# Extract the key and value by removing the longest matching suffix and prefix respectively
-			key="${param%%=*}"
-			value="${param#*=}"
-			# Declare the variable globally in the script
-			declare -g "$key"="$value"
-		done
+    # Iterate over each key-value pair
+    for param in "${params[@]}"; do
+        # Extract the key and value by removing the longest matching suffix and prefix respectively
+        key="${param%%=*}"
+        value="${param#*=}"
+        # Declare the variable globally in the script
+        declare -g "$key"="$value"
+    done
 
-		runfile="$file""-"$(date '+%Y-%m-%d_%H:%M:%S')"-run"
-		# Now you can access the variables
-		echo "Chunk Size: $chunk_size" >> $runfile
-		echo "Distribution Type: $distributionType" >> $runfile
-		echo "Working Set Ratio: $working_set_ratio" >> $runfile
-		echo "Zone Size: $zone_size" >> $runfile
-		echo "Iterations: $iterations" >> $runfile
-		echo "Number of Zones: $num_zones" >> $runfile
-		echo "Total Chunks: $total_chunks" >> $runfile
+    runfile="$file""-"$(date '+%Y-%m-%d_%H:%M:%S')"-run"
+    # Now you can access the variables
+    {
+        echo "Chunk Size: $chunk_size"
+        echo "Distribution Type: $distributionType"
+        echo "Working Set Ratio: $working_set_ratio"
+        echo "Zone Size: $zone_size"
+        echo "Iterations: $iterations"
+        echo "Number of Zones: $num_zones"
+        echo "Total Chunks: $total_chunks"
+    } >> "$runfile"
 
-		# shellcheck disable=SC2024
-		sudo ./buildDir/src/zncache "$1" "$chunk_size" "$threads" "$file" "$iterations" >> "$runfile"
-
+    # shellcheck disable=SC2024
+    if ! sudo ./buildDir/src/zncache "$1" "$chunk_size" "$threads" "$file" "$iterations" >> "$runfile"; then
+        echo "Run FAILED!"
     fi
 done
 
