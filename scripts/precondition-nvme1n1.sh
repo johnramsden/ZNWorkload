060db7f417ceef10bf2c8ddffd69bdcd86144982
John Ramsden <john@ubc.ramsdenj.ca>
2025-03-27 15:08:21 -0700

    Only loop once

--- scripts/precondition-nvme1n1.sh
+++ scripts/precondition-nvme1n1.sh
@@ -5,7 +5,7 @@ set -e
 sgdisk --zap-all /dev/nvme1n1
 
 fio --name=precondition --filename=/dev/nvme1n1 --direct=1 \
-    --rw=randwrite --bs=64k --size=100% --loops=2 \
+    --rw=randwrite --bs=64k --size=100% --loops=1 \
     --randrepeat=0 --ioengine=libaio \
     --numjobs=1 --group_reporting
 
